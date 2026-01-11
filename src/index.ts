f551be425c4f53cf32e84ea2a51eb1fc6314612c
superruudje <rjhaamke@gmail.com>
2026-01-11 11:11:44 +0100

    chore: removed storybook examples

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import "./styles/index.scss";
 export * from "./components/Alert";
 export * from "./components/Button";
 export * from "./components/Card";
+export * from "./components/Flex";
+export * from "./components/Heading";
+export * from "./components/Icon";
+export * from "./components/Loader";
+export * from "./components/NavigationCard";
